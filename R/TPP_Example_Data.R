4ddcec2f334f8c76baa2566d85b4ba271ded571c
Aaron K <69266045+amkeele2@users.noreply.github.com>
2021-01-05 15:55:24 -0500

    Adding TPP Example Data

---
+++ R/TPP_Example_Data.R
@@ -0,0 +1,17 @@
+#' Yeast Lysate with addition of CyclosporineA OnePotTPP TMT10plex
+#'
+#' Yeast Lysate with addition of CyclosporineA OnePotTPP
+#' TMT10plex Proteome Discoverer (v. 2.2) output
+#'
+#' @docType data
+#'
+#' @usage data(TPPExample)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references Cabrera et al. (2020) J. Am. Soc. Mass Spectrom. 31:217-226
+#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/23979570}{PubMed})
+#'
+#' @source \href{https://pubs.acs.org/doi/pdf/10.1021/jasms.9b00041}{QTL Archive}

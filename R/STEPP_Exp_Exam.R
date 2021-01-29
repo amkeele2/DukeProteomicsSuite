4e1b34df12aaea0798f1ffe7af39114af8da6ef3
Aaron K <69266045+amkeele2@users.noreply.github.com>
2021-01-29 14:40:35 -0500

    Added Example STEPP Data

---
+++ R/STEPP_Exp_Exam.R
@@ -0,0 +1,17 @@
+#' MCF7 - MCF10A Cell Lysate with addition of proteinase K STEPP Phenotype Expression Data TMT10plex
+#'
+#' MCF7 - MCF10A Cell Lysate with addition of proteinase K STEPP Phenotype Expression Data
+#' TMT10plex Proteome Discoverer (v. 2.3) output
+#'
+#' @docType data
+#'
+#' @usage data(STEPPExpressionPhenotypeExample)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references Ma et al. (2018) Anal. Chem. 90:14039-14047
+#' (\href{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6524534/}{PubMed})
+#'
+#' @source \href{https://pubs.acs.org/doi/pdf/10.1021/acs.analchem.8b04122}{QTL Archive}
